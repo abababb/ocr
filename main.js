3c4cc72e0d48f6b20509df7997333e9ab85f3ab0
zzz <tenkanse@hotmail.com>
2018-05-19 17:16:08 +0800

    保存接口返回结果

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const AipOcrClient = require("baidu-aip-sdk").ocr;
 const fs = require('fs');
 const Rx = require('rxjs/Rx');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 
 // 设置APPID/AK/SK
@@ -9,14 +11,13 @@ const API_KEY = "MMGDH4T8Ae4ZQgDK5cKLfa8k";
 const SECRET_KEY = "buopkedYa2TasKT91axAegDbti6N6WUp";
 
 const PATH = 'assets/lingqi';
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'lingqi';
 
 // 新建一个对象，建议只保存一个对象调用服务接口
 let client = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
-
-
 let callApi = (file) => {
   let image = fs.readFileSync(PATH + '/' + file).toString("base64");
-
 
   /*
   // 通用文字识别
@@ -73,33 +74,42 @@ let callApi = (file) => {
   */
 }
 
-let dirFiles = fs.readdirSync(PATH);
-dirFiles.sort((f1, f2) => {
-  f1 = f1.substr(0, f1.length - 4);
-  f2 = f2.substr(0, f2.length - 4);
-  return parseInt(f1) - parseInt(f2);
+
+// Use connect method to connect to the server
+MongoClient.connect(dbUrl,{useNewUrlParser: true}, function(err, client) {
+  assert.equal(null, err);
+
+  const db = client.db(dbName);
+  const collection = db.collection('raw');
+
+  let dirFiles = fs.readdirSync(PATH);
+  dirFiles.sort((f1, f2) => {
+    f1 = f1.substr(0, f1.length - 4);
+    f2 = f2.substr(0, f2.length - 4);
+    return parseInt(f1) - parseInt(f2);
+  });
+
+  let count = 30;// 每日免费接口次数限制
+  let intervalStream = Rx.Observable.interval(3000).take(count);
+  let requestStream = Rx.Observable.from(dirFiles).take(count);
+  requestStream = intervalStream.zip(requestStream, (interval, request) => request);
+  let responseStream = requestStream.flatMap(file => Rx.Observable.fromPromise(callApi(file)));
+  let dbStream = responseStream
+    .zip(requestStream, (response, file) => {return {file: file, data: response}})
+    .flatMap(data => Rx.Observable.fromPromise(collection.insertOne(data)));
+
+  dbStream.zip(requestStream, (dbResult, file) => file)
+    .subscribe(
+        data => {
+          console.log(data);
+          fs.unlinkSync(PATH + '/' + data);
+        },
+        err => {
+          console.log(err);
+        },
+        () => {
+          console.log('完成');
+          client.close();
+        }
+        );
 });
-
-let count = 3;// 每日免费接口次数限制
-let intervalStream = Rx.Observable.interval(3000).take(count);
-let requestStream = Rx.Observable.from(dirFiles).take(count);
-requestStream = intervalStream.zip(requestStream, (interval, request) => request)
-let responseStream = requestStream.flatMap(file => Rx.Observable.fromPromise(callApi(file)));
-
-responseStream.subscribe(
-    data => {
-      console.log(JSON.stringify(data));
-      /*
-         if (data.words_result) {
-         let words = data.words_result.map(info => info.words); 
-         console.log(words);
-         }
-         */
-    },
-    err => {
-      console.log(err);
-    },
-    () => {
-      console.log('完成');
-    }
-    );
